8b2f0f41398d726ae6812120519d98de730260a0
Sébastien GONZALEZ <s.gonzalez@opendecide.com>
2016-09-22 16:52:30 +0200

    site debut suite

--- js/main.js
+++ js/main.js
@@ -1,3 +1,22 @@
  jQuery(document).ready(function($) {
-      $('.slogan').height($(window).height());
+    $(".slogan").height($(window).height());
  });
+
+window.onresize = resize;
+
+function resize() {
+    $(".slogan").height($(window).height());
+}
+
+$("#history").click(function(){
+	$("html, body").animate({
+		scrollTop:$(".template .history").offset().top
+	}, "slow");
+	return false;
+});
+$("#contact-us").click(function(){
+	$("html, body").animate({
+		scrollTop:$(".contact-us").offset().top
+	}, "slow");
+	return false;
+});
